1b68569e0ebdbc665b0e07a871458a27dce0cd0b
Bohan Cheng <47214785+cbh778899@users.noreply.github.com>
2024-07-07 16:43:47 +1000

    Update settings.js
    Signed-off-by: Bohan Cheng <47214785+cbh778899@users.noreply.github.com>

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 export const VERSION = '0.1.2'
 // export const API_ADDRESS = 'http://
 // kirin is the name of the API aggregator server container
-export const LOCAL_API_ADDRESS = 'http://kirin:8000/api'
+export const LOCAL_API_ADDRESS = '/api'
