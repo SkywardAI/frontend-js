43ab9cc4c47d8d1e3c28e54fff6232979ebdc728
cbh778899 <cbh778899@outlook.com>
2024-07-08 14:28:49 +1000

    update api
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- settings.js
+++ settings.js
@@ -1,4 +1,2 @@
 export const VERSION = '0.1.3'
-// export const API_ADDRESS = 'http://
-// kirin is the name of the API aggregator server container
-export const LOCAL_API_ADDRESS = '/api'
+export const API_ADDRESS = '/api'
