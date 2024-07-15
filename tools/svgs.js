49ec27bf81dea588f662b452b8e2a3ab122ce570
cbh778899 <cbh778899@outlook.com>
2024-07-15 14:03:16 +1000

    add new svg
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- tools/svgs.js
+++ tools/svgs.js
@@ -139,6 +139,11 @@ const svgs = {
 	"stop-circle-fill": `
 	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
 		<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.5 5A1.5 1.5 0 0 0 5 6.5v3A1.5 1.5 0 0 0 6.5 11h3A1.5 1.5 0 0 0 11 9.5v-3A1.5 1.5 0 0 0 9.5 5z"/>
+	</svg>`,
+
+	"check": `
+	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+		<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
 	</svg>`
 }
 
