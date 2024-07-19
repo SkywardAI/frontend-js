33cd7b9568aff0e9b9afe0cf028532fe01c10658
cbh778899 <cbh778899@outlook.com>
2024-07-19 19:25:38 +1000

    re-implement training page layout
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

---
+++ components/training-page/faq-page.js
@@ -0,0 +1,3 @@
+export default function loadFAQPage(main) {
+    return null;
+}
---
+++ components/training-page/parameters-page.js
@@ -0,0 +1,7 @@
+export default function loadParametersPage(main) {
+    main.innerHTML = `
+    <div id='outer-links-section' class='vertical-section'></div>
+    <div id='model-dataset-section' class='vertical-section'></div>
+    <div id='adjust-params-section' class='vertical-section'></div>`
+    return null;
+}
