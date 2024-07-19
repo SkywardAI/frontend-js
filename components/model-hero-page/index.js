829d2e0b1e8c3ccbe22b70e876436c5295ee15d7
cbh778899 <cbh778899@outlook.com>
2024-07-19 15:31:41 +1000

    optomise infinite scroll
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- components/model-hero-page/index.js
+++ components/model-hero-page/index.js
@@ -2,8 +2,8 @@ import debounce from '../../tools/debounce.js';
 import getSVG from '../../tools/svgs.js'
 
 const templates = [
-    { name: 'template 1' },
-    { name: 'template 2' },
+    { name: 'template 1', description: 'this is description' },
+    { name: 'template 2', description: 'this is description' },
 ]
 
 let display_templates, card_width = 0, move_length = 0, current_position = 'first';
@@ -24,36 +24,48 @@ export default function createModelHeroPage() {
                 <div class='submit-search icon'>${getSVG('search')}</div>
             </div>
         </form>
-        <div id='display-templates'>${
-            templates.map(e=>{
-                return `<div class='template-card'>${e.name}</div>`
-            }).join('')
-        }</div>
-        <div id='selected-template'></div>
+        <div class='templates-section'>
+            <div class='switch-template last clickable'>${getSVG('caret-right-fill')}</div>
+            <div id='display-templates'>${
+                templates.map(e=>{
+                    return `
+                    <div class='template-card clickable'>
+                        <div class='title'>${e.name}</div>
+                        <div class='description'>${e.description}</div>
+                    </div>`
+                }).join('')
+            }</div>
+            <div class='switch-template next clickable'>${getSVG('caret-right-fill')}</div>
+        </div>
     </div>`
 
     const search_template = document.getElementById('search-template');
     display_templates = document.getElementById('display-templates');
-    // const selected_template = document.getElementById('selected-template');
-
     search_template.onsubmit = submitSearchTemplate;
+
+    // =============================================================
+    // 
+    //                       templates section
+    // 
+    // =============================================================
 
     document.addEventListener("resize", windowResized);
     display_templates.addEventListener('wheel', scrollTemplates);
+    document.querySelector('.templates-section .switch-template.next').onclick = () => adjustScroll(true)
+    document.querySelector('.templates-section .switch-template.last').onclick = () => adjustScroll(false)
 
-    windowResized();
+    calculateWithInfo();
 
     const template_cards = Array.from(document.querySelectorAll('#display-templates .template-card'));
     const append_first_list = template_cards.slice(-2).map(e=>e.cloneNode(true));
-    const append_last_list = template_cards.slice(0,2).map(e=>e.cloneNode(true));
 
     const append_first = append_first_list[0];
-    const append_last = append_last_list[0];
+    const append_last = template_cards[0].cloneNode(true);
 
-    display_templates.append(...append_last_list);
+    display_templates.append(append_last);
     display_templates.firstElementChild.before(...append_first_list);
 
-    display_templates.scrollTo({ left: card_width*2, behavior: 'instant' })
+    display_templates.scrollTo({ left: card_width + move_length, behavior: 'instant' })
 
     const obverver = new IntersectionObserver(entries=>{
         entries.forEach(e=>{
@@ -69,6 +81,12 @@ export default function createModelHeroPage() {
 
     obverver.observe(append_first)
     obverver.observe(append_last)
+
+    // =============================================================
+    // 
+    //                          return
+    // 
+    // =============================================================
 
     return () => {
         document.removeEventListener("resize", windowResized);
@@ -89,7 +107,7 @@ const adjustScroll = debounce((direction)=>{
     if(current_position === 'last' && direction) {
         display_templates.scrollTo({ left: move_length, behavior: 'instant' })
     } else if(current_position === 'first' && !direction) {
-        display_templates.scrollTo({ left: display_templates.scrollWidth  - card_width*3 - move_length, behavior: 'instant' })
+        display_templates.scrollTo({ left: display_templates.scrollWidth  - card_width*2 - move_length, behavior: 'instant' })
         // return;
     }
 
@@ -97,10 +115,12 @@ const adjustScroll = debounce((direction)=>{
     display_templates.scrollLeft += multiplier;
 }, 20)
 
-function windowResized() {
+function calculateWithInfo() {
     card_width = display_templates.firstElementChild.clientWidth;
-    move_length = card_width * 0.6;
+    move_length = card_width * 0.6 + 20;
 }
+
+const windowResized = debounce(calculateWithInfo , 20);
 
 function scrollTemplates(evt) {
     adjustScroll(evt.deltaY > 0)
