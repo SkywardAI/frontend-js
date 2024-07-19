4952a874d788841bd9f8a3f57697569cd666db27
cbh778899 <cbh778899@outlook.com>
2024-07-19 13:04:14 +1000

    add search svg
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- tools/svgs.js
+++ tools/svgs.js
@@ -144,6 +144,11 @@ const svgs = {
 	"check": `
 	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
 		<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+	</svg>`,
+
+	"search": `
+	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+		<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
 	</svg>`
 }
 
