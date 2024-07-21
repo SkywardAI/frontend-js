06e3292e69f36d1ccd39f6daf83617f50580d335
cbh778899 <cbh778899@outlook.com>
2024-07-21 13:36:02 +1000

    implemented advanced settings
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- components/training-page/parameters-page.js
+++ components/training-page/parameters-page.js
@@ -1,4 +1,36 @@
-let main_form;
+import createDialog from "../../tools/dialog.js";
+import showMessage from "../../tools/message.js"
+
+const settings = {
+    output_dir: "WANDB_NAME",
+    evaluation_strategy: "steps",
+    do_eval: true,
+    optim: "adamw_8bit",
+    per_device_train_batch_size: 8,
+    gradient_accumulation_steps: 2,
+    per_device_eval_batch_size: 8,
+    log_level: "debug",
+    logging_steps: 100,
+    learning_rate: 8e-6,
+    eval_steps: 100,
+    save_steps: 100,
+    save_strategy: "epoch",
+    num_train_epochs: 1,
+    warmup_ratio: 0.1,
+    lr_scheduler_type: "linear",
+    beta: 0.1,
+    max_length: 1024,
+    report_to: "wandb",
+    run_name: 'WANDB_NAME'
+}
+
+const setting_value_type = {};
+for(const key in settings) setting_value_type[key] = typeof settings[key];
+
+// TODO: load settings from local
+
+const [ advanced_settings_dialog, controller ] = createDialog();
+let main_form, init = false;
 
 export default function loadParametersPage(main) {
     main.innerHTML = `
@@ -54,7 +86,7 @@ export default function loadParametersPage(main) {
                 <div class='title'>Some Random Setting 5</div>
                 <input type='text'>
             </div>
-            <div class='advanced clickable'>
+            <div id='advanced-setting' class='advanced clickable'>
                 <div class='title'>Advanced Settings</div>
             </div>
         </section>
@@ -71,7 +103,82 @@ export default function loadParametersPage(main) {
         console.log(getEntryValues());
     }
 
+    createAdvancedSettingPage();
+    document.getElementById('advanced-setting').onclick = controller.showModal;
+
     return null;
+}
+
+function createAdvancedSettingPage() {
+    if(init) return;
+
+    const table = document.createElement('table');
+    table.innerHTML = 
+    `<thead>
+        <tr>
+            <th scope='col'>Key</th>
+            <th scope='col'>Value</th>
+        </tr>
+    </thead>`
+
+    const tbody = document.createElement('tbody');
+    for(const key in settings) {
+        const value = settings[key];
+        tbody.appendChild(createAdvancedSettingRow(setting_value_type[key], key, value))
+    }
+    table.appendChild(tbody);
+
+    const dialog_main = document.createElement('div');
+    dialog_main.className = 'advanced-setting-table';
+    dialog_main.onclick = evt => evt.stopPropagation();
+    dialog_main.appendChild(table);
+
+    advanced_settings_dialog.appendChild(dialog_main);
+
+    init = true;
+}
+
+function createAdvancedSettingRow(type, key, value) {
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${key}</td>`;
+
+    const column = document.createElement('td');
+
+    const input = document.createElement('input');
+    if(type === 'boolean') {
+        const show_value = document.createElement('div');
+        show_value.className = 'show-boolean-value';
+        show_value.textContent = value;
+
+        input.type = 'checkbox';
+        input.className = 'clickable'
+        input.checked = value;
+        input.onchange = evt => {
+            const value = evt.target.checked;
+            show_value.textContent = value;
+            settings[key] = value;
+            showMessage(`"${key}" set to ${value}`);
+        }
+        column.appendChild(show_value);
+    } else {
+        input.type = 'text';
+        input.value = value;
+        input.onchange = evt => {
+            const value = settingValueParser(type, evt.target.value);
+            if(value === null) {
+                showMessage(`The entry "${key}" must be type of ${type}!`, { type:'warn' })
+                evt.target.value = settings[key]
+                return;
+            }
+            settings[key] = value;
+            evt.target.value = value;
+            showMessage(`"${key}" set to ${value}`);
+        }
+    }
+
+    column.appendChild(input);
+    row.appendChild(column);
+    return row;
 }
 
 function getEntryValues() {
@@ -88,4 +195,13 @@ function getEntryValues() {
 
 function startTraining(evt) {
     evt.preventDefault();
+}
+
+function settingValueParser(type, value) {
+    if(type === 'number') {
+        value = +value;
+        return isNaN(value) ? null : value;
+    } else {
+        return value || null;
+    }
 }
