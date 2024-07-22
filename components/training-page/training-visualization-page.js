20a3b991d098f4b4a0682dfcca9f7bfe81547eb7
cbh778899 <cbh778899@outlook.com>
2024-07-22 17:31:24 +1000

    implement tensorboard microservice
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- components/training-page/training-visualization-page.js
+++ components/training-page/training-visualization-page.js
@@ -1,4 +1,10 @@
 export default function loadTrainingVisualizationPage(main) {
     main.innerHTML = '';
+    
+    const training_frame = document.createElement('iframe');
+    training_frame.src = '/training-visualization/';
+    training_frame.className = 'seamless-frame'
+    main.appendChild(training_frame);
+
     return null;
 }
