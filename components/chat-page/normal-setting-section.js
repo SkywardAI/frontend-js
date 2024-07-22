2aec4bf5b8369a3b4d61ff3daac5ac9cdd8d33c0
cbh778899 <cbh778899@outlook.com>
2024-07-22 18:43:51 +1000

    add option button
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- components/chat-page/normal-setting-section.js
+++ components/chat-page/normal-setting-section.js
@@ -1,39 +1,27 @@
 /**
  * create a normal setting section
- * @param { "text" | "select" } type What type this section should contains
+ * @param { "text" | "select" | "button" } type What type this section should contains
  * @param {String} title Name of this secction, apperas on top
  * @param {Function} callback Callback function takes one parameter of updated value of this section
  * @returns { [HTMLElement, Function, Function] } 
  *  Returns an array containes necessary fields  
- *  **Index 0**: The HTMLElement
- *  **Index 1**: The setter function to update value
- *  **Index 2**: The setter function to update args
+ *  **Index 0**: The HTMLElement  
+ *  **Index 1**: The setter function to update value  
+ *  **Index 2**: The setter function to update args  
  */
-export default function normalSettigSection(type, title, callback, ...args) {
+export default function normalSettigSection(type, title, callback = null, ...args) {
     const section = document.createElement('div');
     section.className = 'setting-section'
     section.innerHTML = `<div class='title'>${title}</div>`;
 
     let input_like;
-    if(type === 'text') {
-        input_like = document.createElement('input');
-        input_like.type = 'text';
-    } else if(type === 'select') {
-        input_like = document.createElement('select');
-        args[0].forEach(({value, title})=>{
-            const option = document.createElement('option');
-            option.value = value;
-            option.textContent = title || value;
-            input_like.appendChild(option);
-        })
-    }
-
-    function setter(value) {
-        input_like.value = value;
-    }
 
     function setArgs(...new_args) {
         args = new_args;
+        loadArgs();
+    }
+
+    function loadArgs() {
         if(type === 'select') {
             input_like.innerHTML = '';
             args[0].forEach(({value, title})=>{
@@ -42,11 +30,27 @@ export default function normalSettigSection(type, title, callback, ...args) {
                 option.textContent = title || value;
                 input_like.appendChild(option);
             })
+        } else if(type === 'button') {
+            input_like.onclick = args[0];
+            input_like.classList.add('clickable')
         }
+    }
+    
+    if(type === 'select') {
+        input_like = document.createElement('select');
+    } else {
+        input_like = document.createElement('input');
+        input_like.type = type;
+    }
+
+    loadArgs();
+
+    function setter(value) {
+        input_like.value = value;
     }
 
     input_like.onchange = () => {
-        callback(input_like.value);
+        callback && callback(input_like.value);
     }
 
     section.appendChild(input_like)
