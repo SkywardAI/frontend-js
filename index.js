d1c39f1464a7c931a6d46fbfd31db56bd5cea9e4
cbh778899 <cbh778899@outlook.com>
2024-07-25 17:17:38 +1000

    define pages needs to login first
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ import createSideBar from "./components/sidebar.js";
 import createChatPage from "./components/chat-page/index.js";
 import createTrainingPage from "./components/training-page/index.js";
 import createModelHeroPage from "./components/model-hero-page/index.js";
+import useUser from "./global/useUser.js";
+import showMessage from "./tools/message.js";
+import createAccountPage from "./components/account-page/index.js";
 
 let last_selected_page = '';
 let componetDismount = null;
 
+let logged_in = false;
+useUser(user=>{
+    logged_in = user.logged_in;
+})
+
 function switchSelectedPage(page) {
     if(page === last_selected_page) return;
+    if(!logged_in) {
+        if(/^(training)$/.test(page)) {
+            showMessage('Please login to use this function.', { type: 'warn' });
+            createAccountPage();
+            return;
+        }
+    }
+
     document.getElementById('main').innerHTML = '';
     componetDismount && componetDismount();
     componetDismount = null;
